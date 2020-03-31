8029e42de401b35de65c8bb2c232dbe80af7610e
David Wu <ji3g4kami@gmail.com>
2020-03-31 09:22:45 +0100

    works in release mode

---
+++ CocoaPodsWithCI/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CocoaPodsWithCI
+//
+//  Created by 登秝吳 on 2020/3/31.
+//  Copyright © 2020 登秝吳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+  @IBAction func buttonPressed(_ sender: UIButton) {
+    print("Hello")
+  }
+}
+
